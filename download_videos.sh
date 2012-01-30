60acac6a916137d03bf67125c926504e296b7fa1
Robin Smidsrød <robin@smidsrod.no>
2012-01-30 19:48:09 +0100

    Added a M4V-encoded example video that works

---
+++ download_videos.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget 'http://www.presentingperl.org/opw2012/doing-the-jitterbug/Jonathan-Leto-Doing-the-Jitterbug.flv'
+wget 'http://www.presentingperl.org/yn2010/icpan/MarkJubenville-OlafAlders-iCPAN.m4v'
